967516fa807478bd899e27b0cf18c7eff9e180eb
brayanfarias <bpjfarias@gmail.com>
2023-07-04 21:48:19 -0300

    add spring required files

---
+++ src/main/java/br/com/airequestgenerator/framework/AiRequestGeneratorApplication.java
@@ -0,0 +1,13 @@
+package br.com.airequestgenerator.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiRequestGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiRequestGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/airequestgenerator/AiRequestGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.airequestgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiRequestGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
